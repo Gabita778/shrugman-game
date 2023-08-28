639bbe3a80d38a394f594911a18235a0ca7ffcc9
Gabriella <icaro7.78@gmail.com>
2023-08-28 17:10:43 +0200

    ordering the ideas

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ class ShrugmanGame {
     constructor(word) {
         this.word = word.toLowerCase(); //movie or book
 
-        // this two propieties will build my shrugman
+        // this two proprieties will build my shrugman
         this.attempts = 0; // how many attempts has use the user
         this.maxAttempts = 10; // all attempts user has
 
@@ -17,6 +17,25 @@ class ShrugmanGame {
 
 
 // what else in my list?
+// player one: give the secret word
+// player two: guess the secret word
+// who to apply the guesses
 
 // Where does the list of words come from? thinking.....
 // R: everything must come from my class
+
+// I need to do something with the symbols a method maybe?
+
+// How do I create the underscores!!!!!?
+
+// possible methods:
+
+    // Replace letters with underscores thinking in the spaces,
+    // Checking if the guessed letter is in the word,  return this.word.includes(letter);
+    // Counting attempts
+    // Replace underscore for letters or symbol maybe this is a IF situation or another possible solution this.guesses.push(letter);
+    // occurrences of the letter are revealed when the guess is correct
+    // telling that the letter was already guess this.guesses.includes(letter)
+
+    // Once the game is over, print a list of all games played, and whether they were a win or a loss
+    // where do I place this box? this mus be an Array
