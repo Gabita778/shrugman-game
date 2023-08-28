3f964f44b641140823ad27fd54761c40df2aed50
Gabriella <icaro7.78@gmail.com>
2023-08-28 17:04:17 +0200

    ordering the ideas

--- index.js
+++ index.js
@@ -4,10 +4,19 @@ const prompt = require('prompt-sync')({ sigint: true });
 class ShrugmanGame {
     constructor(word) {
         this.word = word.toLowerCase(); //movie or book
+
+        // this two propieties will build my shrugman
         this.attempts = 0; // how many attempts has use the user
         this.maxAttempts = 10; // all attempts user has
+
         this.gameOver = false; // lose or win scenario
     }
 }
 
+// Answering my own questions?
+
+
 // what else in my list?
+
+// Where does the list of words come from? thinking.....
+// R: everything must come from my class
