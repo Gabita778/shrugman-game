53d98ee4dfec7d3c89e35bfcf00988a36af2236d
Gabriella <icaro7.78@gmail.com>
2023-08-28 16:44:08 +0200

    this one is goint to be hard

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const prompt = require('prompt-sync')({ sigint: true });
+
+// making a list of properties in my class
+class ShrugmanGame {
+    constructor(word) {
+        this.word = word.toLowerCase(); //movie or book
+        this.attempts = 0; // how many attempts has use the user
+        this.maxAttempts = 10; // all attempts user has
+        this.gameOver = false; // lose or win scenario
+    }
+}
+
+// what else in my list?
