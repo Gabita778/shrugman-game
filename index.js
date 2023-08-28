f8a91a8f797bc41c7da4ce5f1070c398e8e2d65b
Gabriella <icaro7.78@gmail.com>
2023-08-28 21:28:14 +0200

    I'm building the code in the dark

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 const prompt = require('prompt-sync')({ sigint: true });
 
+
+let n = prompt("guess the word!?");
+
+
+
 // making a list of properties in my class
-class ShrugmanGame {
+class ShrugManGame {
     constructor(word) {
         this.word = word.toLowerCase(); //movie or book
 
-        // this two proprieties will build my shrugman
-        this.attempts = 0; // how many attempts has use the user
-        this.maxAttempts = 10; // all attempts user has
+        // this two proprieties will build my shrugman? ¯\_(:/)_/¯
+        this.attempts = this.attempts ; // (start at 0) how many attempts has use the user
+        this.maxAttempts = maxAttempts; // (end at 10) all attempts user has
 
         this.gameOver = false; // lose or win scenario
     }
+
+    isLetterHere(letter) {
+        // Check if the guessed letter is in the word
+        return this.word.includes(letter);
+      }
 }
+
+const game = new ShrugManGame("Everything Everywhere All at Once");
+console.log(game);
 
 // Answering my own questions?
 
@@ -31,18 +44,25 @@ class ShrugmanGame {
 // possible methods:
 
     // Replace letters with underscores thinking in the spaces,
-    // Checking if the guessed letter is in the word,  return this.word.includes(letter);
+
+       // return this.word.replace(/[a-z]/g, '_'); I don't like much regex but that maybe will work
+    //    I not sure it will count spaces
+
+   // Checking if the guessed letter is in the word,
+
+      //isLetterIn function() that return this.word.includes(letter);
 
 
-    // Counting attempts
+    
     // Replace underscore for letters or symbol maybe this is a IF situation or another possible solution this.guesses.push(letter);
     // occurrences of the letter are revealed when the guess is correct
     // telling that the letter was already guess this.guesses.includes(letter)
+
     // how many attempts left ?
+    // Counting attempts
         
-    ShrugmanGame.attemptsLeft() {
-        return this.maxAttempts - this.attempts;
-      }
+      // attemptsLeft  function() that  return this.maxAttempts - this.attempts ;
+      
 
     // Once the game is over, print a list of all games played, and whether they were a win or a loss
     // where do I place this box? this mus be an Array
