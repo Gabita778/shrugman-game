a6d987efe728538613bfc797e2e84b24c540f009
Gabriella <icaro7.78@gmail.com>
2023-08-29 14:29:52 +0200

    starting to look better, this version works

--- index.js
+++ index.js
@@ -141,42 +141,6 @@ function displayGameState(game) {
 
 displayGameState(game);
 
-// Answering my own questions?
+// new questions?
 
-
-// what else in my list?
-// player one: give the secret word is the code
-// player two: guess the secret word is the prompt
-// where to apply the guesses? to the object in the class
-
-// Where does the list of words come from? thinking.....
-// R: everything must come from my class, no better a function 
-
-// I need to do something with the symbols a method maybe?
-
-// How do I create the underscores!!!!!?
-
-// possible methods:
-
-    // Replace letters with underscores thinking in the spaces,
-
-       // return this.word.replace(/[a-z]/g, '_'); I don't like much regex but that maybe will work
-    //    I not sure it will count spaces
-
-   // Checking if the guessed letter is in the word, =========done==========
-      //isLetterIn function() that return this.word.includes(letter);
-      // telling that the letter was already guess this.guesses.includes(letter)
-
-    
-    // Replace underscore for letters or symbol maybe this is a IF situation or another possible solution this.guesses.push(letter);
-    // occurrences of the letter are revealed when the guess is correct
-
-   
-
-    // how many attempts left ? ===========done=======
-    // Counting attempts
-    // attemptsLeft  function() that  return this.maxAttempts - this.attempts ;
-      
-
-    // Once the game is over, print a list of all games played, and whether they were a win or a loss
-    // where do I place this box? this mus be an Array
+// how to loop the questions?
